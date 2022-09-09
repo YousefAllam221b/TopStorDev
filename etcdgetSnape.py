44f17e9721a28a2525795b96dc958abe4b0ac069
dlksj.ewjf <dklljfd@sldjf.com>
2022-09-09 18:12:20 +0300

    updated etcdgetSnape

--- etcdgetSnape.py
+++ etcdgetSnape.py
@@ -1,35 +1,20 @@
 #!/bin/python3.6
-import subprocess,sys
-
+import subprocess,sys,os
 def etcdget(key, prefix=''):
 	if(prefix == '--prefix'):
-		netstat = ['netstat','-ant']
-		grepPort= ['grep',':2378']
-		countGrep= ['wc','-l']
-		proc1 = subprocess.Popen(netstat,stdout=subprocess.PIPE)
-		proc2 = subprocess.Popen(grepPort,stdin=proc1.stdout,stdout=subprocess.PIPE)
-		proc3 = subprocess.Popen(countGrep,stdin=proc2.stdout,stdout=subprocess.PIPE)
-		out=proc3.communicate()
-		out = str(out)[3:][:-10]
-		if (int(out) != 0):
+		proc1 = os.popen('netstat -ant | grep 2378 | wc -l')
+		wc_output = int(proc1.read())
+		if (wc_output != 0):
+			import etcdgetlocal
 			print('Port 2378 is Open. Running ./etcdgetlocal.py')
-			getResources=['pcs','resource','show','CC']
-			proc4 = subprocess.run(getResources,stdout=subprocess.PIPE)
-			resources=proc4.stdout.decode('utf-8')
-			resources = "".join(resources).split('\n')
-			resourcesAttributes = resources[1]
-			attributes= resourcesAttributes.split(" ")
-			ip=attributes[4]
-			ip=ip.split('=')[1]
-			etcdgetlocal=['./etcdgetlocal.py',ip,'leader','--prefix']
-			finalProc = subprocess.run(etcdgetlocal,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-			print(finalProc)
+			proc2 = os.popen('pcs resource show CC')
+			resources = "".join(proc2.read()).split('\n')
+			ip = resources[1].split(" ")[4].split('=')[1]
+			etcdgetlocal.etcdget(ip, key, prefix)
 		else:
+			import etcdget
 			print('Port 2378 is closed. Running ./etcdget.py')
-			etcdget=['./etcdget.py','leader','--prefix']
-			finalProc=subprocess.run(etcdget,stdout=subprocess.PIPE)
-			print(finalProc.stdout)
-
+			etcdget.etcdget(key, prefix)
 	
 if __name__=='__main__':
  etcdget(*sys.argv[1:])
