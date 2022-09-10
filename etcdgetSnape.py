c62e9cb383a35286de557b9550d2f13e170032ee
dlksj.ewjf <dklljfd@sldjf.com>
2022-09-11 01:22:53 +0300

    fixed etcdgetSnape.py to handle passed parameters

--- etcdgetSnape.py
+++ etcdgetSnape.py
@@ -1,20 +1,19 @@
 #!/bin/python3.6
 import subprocess,sys,os
 def etcdget(key, prefix=''):
-	if(prefix == '--prefix'):
-		proc1 = os.popen('netstat -ant | grep 2378 | wc -l')
-		wc_output = int(proc1.read())
-		if (wc_output != 0):
-			import etcdgetlocal
-			print('Port 2378 is Open. Running ./etcdgetlocal.py')
-			proc2 = os.popen('pcs resource show CC')
-			resources = "".join(proc2.read()).split('\n')
-			ip = resources[1].split(" ")[4].split('=')[1]
-			etcdgetlocal.etcdget(ip, key, prefix)
-		else:
-			import etcdget
-			print('Port 2378 is closed. Running ./etcdget.py')
-			etcdget.etcdget(key, prefix)
+	proc1 = os.popen('netstat -ant | grep 2378 | wc -l')
+	wc_output = int(proc1.read())
+	if (wc_output != 0):
+		import etcdgetlocal
+		print('Port 2378 is Open. Running ./etcdgetlocal.py')
+		proc2 = os.popen('pcs resource show CC')
+		resources = "".join(proc2.read()).split('\n')
+		ip = resources[1].split(" ")[4].split('=')[1]
+		etcdgetlocal.etcdget(ip, key, prefix)
+	else:
+		import etcdget
+		print('Port 2378 is closed. Running ./etcdget.py')
+		etcdget.etcdget(key, prefix)
 	
 if __name__=='__main__':
  etcdget(*sys.argv[1:])
