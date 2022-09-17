82fc8fe059ecc0d8ab2b1da979aa1916d3cb9371
dlksj.ewjf <dklljfd@sldjf.com>
2022-09-17 16:31:48 +0300

    fixed etcdgetu.py and made etcdgetus.py

--- etcdgetus.py
+++ etcdgetus.py
@@ -1,7 +1,6 @@
 #!/bin/python3.6
 import subprocess,sys,os
 def etcdget(key, prefix=''):
-	proc1 = os.popen('netstat -ant | grep 2378 | wc -l')
 	proc = os.popen('netstat -ant | grep 2378')
 	allLines = proc.read().splitlines()
 	openCount = 0
@@ -9,19 +8,15 @@ def etcdget(key, prefix=''):
 		lineSplitted = ' '.join(line.split()).split(' ')
 		port = lineSplitted[3].split(':')[1]
 		if (port == '2378'):
-			print(port)
 			openCount += 1
-	#wc_output = int(proc1.read())
 	if (openCount != 0):
 		import etcdgetlocal
-		print('Port 2378 is Open. Running ./etcdgetlocal.py')
 		proc2 = os.popen('pcs resource show CC')
 		resources = "".join(proc2.read()).split('\n')
 		ip = resources[1].split(" ")[4].split('=')[1]
 		etcdgetlocal.etcdget(ip, key, prefix)
 	else:
 		import etcdget
-		print('Port 2378 is closed. Running ./etcdget.py')
 		etcdget.etcdget(key, prefix)
 	
 if __name__=='__main__':
